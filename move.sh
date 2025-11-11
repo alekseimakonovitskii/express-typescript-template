c6c082a921565761a0543ca0b893389a8493af5b
alekseimakonovitskii <71717332+alekseimakonovitskii@users.noreply.github.com>
2025-11-12 00:34:05 +0300

    Update move.sh

--- move.sh
+++ move.sh
@@ -6,12 +6,17 @@ TARGET_DIR="$(pwd)"
 shopt -s dotglob nullglob
 
 for item in "$SCRIPT_DIR"/* "$SCRIPT_DIR"/.*; do
-  if [[ "$(basename "$item")" == "." || "$(basename "$item")" == ".." || "$(basename "$item")" == "$SCRIPT_NAME" ]]; then
+  base="$(basename "$item")"
+  if [[ "$base" == "." || "$base" == ".." || "$base" == "$SCRIPT_NAME" ]]; then
     continue
   fi
   mv "$item" "$TARGET_DIR"/
 done
 
+if [ -f "$TARGET_DIR/README.md" ]; then
+  rm -f "$TARGET_DIR/README.md"
+fi
+
 rm -rf "$SCRIPT_DIR"
 
-echo "Template is ready ✅"
+echo "✅ Template is ready!"
